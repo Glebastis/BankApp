aaf77b82b3e512076866647bed4d85fde2420c84
Gleb <babenkogleb@gmail.com>
2021-01-19 14:42:12 +0300

    currency settings

--- BankApp/ViewController.swift
+++ BankApp/ViewController.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountLabel: UILabel?
+    
+    var amount: Float = 10000{
+        didSet {
+            updateAmountLabel()
+        }
+    }
+    
+    func updateAmountLabel() {
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "₽"
+        
+        let number = NSNumber(value: amount)
+        amountLabel?.text = formatter.string(from: number)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateAmountLabel()
+        
     }
 
 
